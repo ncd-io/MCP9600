7c1aa18a40daa486dd0d21e3c6214e94753b96d4
zaphod-42 <trey@tomadoh.com>
2017-09-19 11:14:56 -0500

    Update mcp9600.ino

--- Particle/mcp9600.ino
+++ Particle/mcp9600.ino
@@ -11,8 +11,9 @@ void setup() {
 
 int last_checked = 0;
 void loop() {
+    //This delay is necessary to give the ADC time to convert the data, it could possibly be shortened based on resolution
     int now = millis();
-    if(now-last_checked > 1000){
+    if(now-last_checked > 250){
         last_checked = now;
         tempSens.readTemp();
     }
